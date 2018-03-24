6212129776025dc507c3851119e0380b6fd28b88
liujian <449667481@qq.com>
2018-03-24 23:14:07 +0800

    update

--- src/main/resources/public/js/index.js
+++ src/main/resources/public/js/index.js
@@ -1,89 +1,347 @@
 
 var Colors=[];
-var Trail={
-		isShow: true,
-		isPoint: true,
-		isLine: true,
-		isDashed: true,
-		isShape: true,
-		isInfo: true,
-		point: [{
+
+var Config = {
+		defulatColor: [226, 119, 40],
+		defulatTimeFormat: "yyyy-MM-dd HH:mm:ss",
+		
+		select: 0,
+		shipsSelect: [],
+		shipsForm:{
+			name: "123"
+		},
+		
+		msgEvent: "",
+		
+		isTrailShow: false,
+		isTrailShowPoint: false,
+		isTrailShowLine: false,
+		isTrailShowDashed: false,
+		isTrailShowShape: false,
+		isTrailShowInfo: false,
+		trailPoint: [{
 			color: "",
-			size: "",
-			data:[]
+			size: ""
 		}],
-		line: [],
-		dashed: [],
-		shape: [],
-		info: []
+		trailLine: [],
+		trailDashed: [],
+		trailShape: [],
+		trailInfo: []
+		
 };
-var Ships=[];
-var ShipsData={};
+
+var Ships=[{
+	mmsi: 0,
+	name: "1号船",
+	paths:[],
+	trailColor: "",
+	trailSize: "",
+	data: [{lat:30.1976, lon:124.9822, real:true, time: "2018-03-24 12:00:00", speed: 11, cos: 4, head:359,mmsi:0,name:"1" },
+			{lat:30.1977, lon:124.9822, real:true, time: "2018-03-24 12:00:01", speed: 11, cos: 4, head:359,mmsi:0,name:"1" },
+			{lat:30.1978, lon:124.9822, real:true, time: "2018-03-24 12:00:02", speed: 11, cos: 4, head:359,mmsi:0,name:"1" },
+			{lat:30.1979, lon:124.9822, real:true, time: "2018-03-24 12:00:03", speed: 11, cos: 4, head:359,mmsi:0,name:"1" },
+			{lat:30.198, lon:124.9822, real:true, time: "2018-03-24 12:00:04", speed: 11, cos: 4, head:359,mmsi:0,name:"1" },
+			{lat:30.1981, lon:124.9822, real:true, time: "2018-03-24 12:00:05", speed: 11, cos: 4, head:359,mmsi:0,name:"1", event: function(){console.log("time event!");} },
+			{lat:30.1982, lon:124.9822, real:true, time: "2018-03-24 12:00:06", speed: 11, cos: 4, head:359,mmsi:0,name:"1" },
+			{lat:30.1983, lon:124.9822, real:true, time: "2018-03-24 12:00:07", speed: 11, cos: 4, head:359,mmsi:0,name:"1" },
+			{lat:30.1984, lon:124.9822, real:true, time: "2018-03-24 12:00:08", speed: 11, cos: 4, head:359,mmsi:0,name:"1" },
+			{lat:30.1985, lon:124.9822, real:true, time: "2018-03-24 12:00:09", speed: 11, cos: 4, head:359,mmsi:0,name:"1" },
+			{lat:30.1986, lon:124.9822, real:true, time: "2018-03-24 12:00:10", speed: 11, cos: 4, head:359,mmsi:0,name:"1" }
+
+	]
+}];
+var CurShipInfo = {
+		
+};
+var TimeEvent = {
+		
+};
+
 var TimeControl={
 		speed: 1,
 		startTime: '',
 		video: [] 
 };
-var ArGis={
-		baseUrl: window.location.href.replace("index.html","")+"arcgis_js_api/library/3.16",
+ArGis={
 		map: "",
+		view: "",
 		center: [124, 30],
 		initData: function(){
 			//加载数据
+			
+			//ship
+			for (var i = 0; i < Ships.length; i++) {
+				var ship = Ships[i];
+				var select = {
+						value: i,
+						label: ship.name
+				};
+				Config.shipsSelect.push(select);
+				//select
+				if(Config.select == i){
+					for (var j = 0; j < ship.data.length; j++) {
+						CurShipInfo[ship.data[j].time] = ship.data[j];
+					}
+				}
+				//event
+				for (var j = 0; j < ship.data.length; j++) {
+					if(ship.data[j].event){
+						var events = TimeEvent[ship.data[j].time];
+						if(!events){
+							events = [];
+						}
+						events.push(ship.data[j]);
+						TimeEvent[ship.data[j].time] = events;
+					}
+				}
+			}
 		},
 		initMap: function(){
 			//加载工具
 			
 			//绘制碰撞点
-			Draws.point(124, 33,[226, 119, 40], 10, {
-				time: "",
-				lon: 124,
-				lat: 33,
-				content: "test"
-			},{
-				title:"碰撞点",
-				content:"碰撞时间：${time}<br/>碰撞经度：${lon}<br/>碰撞纬度：${lat}<br/>碰撞描述：${content}"
-			});
+			Utils.drawPoint({
+						lon: 124,
+						lat: 33,
+						color: [226, 119, 40],
+						size: 10,
+						attr: {
+							time: "",
+							lon: 124,
+							lat: 33,
+							content: "test"
+						},
+						template: {
+							title:"碰撞点",
+							content:[{
+							        type: "fields",
+							        fieldInfos: [{
+							          fieldName: "time",
+							          label: "碰撞时间"
+							        }, {
+							          fieldName: "lon",
+							          label: "碰撞经度"
+							        }, {
+								          fieldName: "lat",
+								          label: "碰撞纬度"
+							        }, {
+								          fieldName: "content",
+								          label: "碰撞描述"
+							        }]
+							      }]
+						}
+					});
 		},
 		initTrail: function(){
-			if(Trail.isShow){
-				this.drawTrail();
-			}else{
-				this.clearTrail();
-			}
+			this.drawTrail();
 		},
 		initShips: function(){
 			
 		},
 		initTimeLine: function(){
+			//视频
+			var video = document.getElementById("video");
+			video.onplay = function(evt){
+				PlayController.handlePlay();
+			};
+			video.onpause = function(evt){
+				PlayController.handlePaused();
+			};
+			video.onended = function(evt){
+				PlayController.curPlayTime = 0;
+				PlayController.handlePaused();
+			};
+			video.ontimeupdate = function(evt){
+//				console.log(video.currentTime);
+			};
+			video.onwaiting = function(){
+				console.log("onwaiting");
+			};
+			//插件
+			var minTime = "";
+			var maxTime = "";
+			for (var i = 0; i < Ships.length; i++) {
+				var ship = Ships[i];
+				for (var j = 0; j < ship.data.length; j++) {
+					if(!minTime){
+						minTime = ship.data[j].time;
+					}else{
+						if(minTime > ship.data[j].time){
+							minTime = ship.data[j].time;
+						}
+					}
+					if(!maxTime){
+						maxTime = ship.data[j].time;
+					}else{
+						if(maxTime < ship.data[j].time){
+							maxTime = ship.data[j].time;
+						}
+					}
+				}
+			}
+			var endTime = Math.floor((new Date(maxTime.replace("-","/")).getTime() - new Date(minTime.replace("-","/")).getTime())/1000);
 			
+			PlayController.endTime= endTime;
+			PlayController.paused= true;
+			PlayController.initPlay();
+			PlayController.emitPaused = function(){
+				video.pause();
+			};
+			PlayController.emitPlay = function(){
+				video.play();
+			};
+			PlayController.emitRePlay = function(){
+				video.currentTime = 0;
+				video.play();
+			};
+			PlayController.emitUpdateTime = function(updateTime){
+				video.currentTime += updateTime;
+			};
+			PlayController.timePointEvent = function(timePoint){
+				var temp = Utils.formatDate(Math.floor(new Date(minTime.replace("-","/")).getTime()/1000) + Math.floor(timePoint), Config.defulatTimeFormat);
+				Utils.updateShipInfo(temp);
+				if(TimeEvent[temp]){
+					for (var i = 0; i < TimeEvent[temp].length; i++) {
+						if(typeof TimeEvent[temp][i].event == "function"){
+							TimeEvent[temp][i].event();
+						}
+					}
+				}
+			};
 		},
 		drawTrail: function(){
-			this.showTrailPoint();
-			this.showTrailLine();
-			this.showTrailDashed();
-			this.showTrailShape();
-		},
-		clearTrail: function(){
-			this.clearTrailPoint();
-			this.clearTrailLine();
-			this.clearTrailDashed();
-			this.clearTrailShape();
-		},
-		showTrailPoint: function(){
-			if(Trail.isPoint){
-				Draws.point(124,30,[226, 119, 40]);
+			if(Config.isTrailShow){
+				for (var i = 0; i < Ships.length; i++) {
+					var ship = Ships[i];
+					this.showTrailPoint(ship);
+					this.showTrailLine(ship);
+					this.showTrailDashed(ship);
+					this.showTrailShape(ship);
+					this.showTrailInfo(ship);
+				}
+			}else{
+				Config.isTrailShowPoint = false;
+				Config.isTrailShowLine = false;
+				Config.isTrailShowDashed = false;
+				Config.isTrailShowShape = false;
+				Config.isTrailShowInfo = false;
+				this.clearTrail();
 			}
 		},
-		showTrailLine: function(){
-			
+		clearTrail: function(){
+			Utils.removeGraphicsByType("trail_point");
+			Utils.removeGraphicsByType("trail_line");
+			Utils.removeGraphicsByType("trail_dash");
+			Utils.removeGraphicsByType("trail_shape");
+			Utils.removeGraphicsByType("trail_info");
 		},
-		showTrailDashed: function(){
-			
+		showTrailPoint: function(ship){
+			//移除原来的点
+			Utils.removeGraphicsByType("trail_point");
+			//新加
+			if(Config.isTrailShowPoint){
+				for (var i = 0; i < ship.data.length; i++) {
+					var point = ship.data[i];
+					if(point.real){
+						Utils.drawPoint({
+							lon: point.lon,
+							lat: point.lat,
+							color: point.color,
+							size: point.size,
+							attr: {mmsi: ship.mmsi, type: "trail_point"},
+							template:{}
+						});
+					}
+				}
+				
+			}
 		},
-		showTrailShape: function(){
-			
+		showTrailLine: function(ship){
+			//移除原来的线
+			Utils.removeGraphicsByType("trail_line");
+			//新加
+			if(Config.isTrailShowLine){
+				var params = {
+						paths: [],
+						color: "",
+						width: "",
+						style: "",
+						attr: {mmsi: ship.mmsi, type: "trail_line"},
+						template: {}
+				};
+				for (var i = 0; i < ship.data.length; i++) {
+					var point = ship.data[i];
+					if(point.real){
+						params.paths.push([point.lon, point.lat]);
+					}
+				}
+				
+				Utils.drawLine(params);
+			}
 		},
-		
+		showTrailDashed: function(ship){
+			//移除原来的虚线
+			Utils.removeGraphicsByType("trail_dash");
+			//新加
+			if(Config.isTrailShowDashed){
+				var params = {
+						paths: [],
+						color: "",
+						width: "",
+						style: "short-dash",
+						attr: {mmsi: ship.mmsi, type: "trail_dash"},
+						template: {}
+				};
+				for (var i = 0; i < ship.data.length; i++) {
+					var point = ship.data[i];
+					if(point.real){
+						params.paths.push([point.lon, point.lat]);
+					}
+				}
+				Utils.drawLine(params);
+			}
+		},
+		showTrailShape: function(ship){
+			//移除原来的虚线
+			Utils.removeGraphicsByType("trail_shape");
+			//新加
+			if(Config.isTrailShowShape){
+				for (var i = 0; i < ship.data.length; i++) {
+					var point = ship.data[i];
+					if(point.real){
+						Utils.drawPoint({
+							lon: point.lon,
+							lat: point.lat,
+							color: point.color,
+							size: point.size,
+							attr: {mmsi: ship.mmsi, type: "trail_shape"},
+							template:{}
+						});
+					}
+				}
+				
+			}
+		},
+		showTrailInfo: function(ship){
+			//移除原来的虚线
+			Utils.removeGraphicsByType("trail_info");
+			//新加
+			if(Config.isTrailShowInfo){
+				for (var i = 0; i < ship.data.length; i++) {
+					var point = ship.data[i];
+					if(point.real){
+						Utils.drawPoint({
+							lon: point.lon,
+							lat: point.lat,
+							color: point.color,
+							size: point.size,
+							attr: {mmsi: ship.mmsi, type: "trail_info"},
+							template:{}
+						});
+					}
+				}
+				
+			}
+		}
 };
 
--- src/main/resources/public/js/utils.js
+++ src/main/resources/public/js/utils.js
@@ -1,35 +1,147 @@
 
-var Draws = {
+var Utils = {
 		trail: function(){
 			
 		},
-		ship: function(){
-			
+		updateShipInfo: function(timePoint){
+			if(CurShipInfo[timePoint]){
+				//绘制ship
+				console.log(CurShipInfo[timePoint].time);
+				//绘制shipInfo
+				
+			}
 		},
-		point: function(lon, lat, color, size, attr, template){
-			require(["esri/geometry/Point", "esri/symbols/SimpleMarkerSymbol", "esri/symbols/SimpleLineSymbol","esri/Color","esri/graphic","esri/InfoTemplate"], 
-					function (Point, SimpleMarkerSymbol, SimpleLineSymbol, Color, Graphic, InfoTemplate) {
-					var geometry = new Point({
-				        x: lon,
-				        y: lat,
-				        spatialReference: {"wkid": 4326 }
-				      });
+		drawPoint: function(params){
+			require(["esri/Color","esri/Graphic"], 
+					function (Color, Graphic) {
+						var geometry = {
+							type: "point",
+						    longitude: params.lon,
+						    latitude: params.lat
+						  };
+						
+				      var symbol  = {
+				  		    type: "simple-marker",  
+				  		    color: params.color ? new Color(params.color) : [226, 119, 40],
+				  		     size: (params.size ? params.size : 8) + "px"
+				  		  };
 
-				      var symbol = new SimpleMarkerSymbol({
-				    	  style: SimpleMarkerSymbol.STYLE_CIRCLE,
-				    	  size: size ? size : 10,
-				    	  color: color ? new Color(color) : [226, 119, 40]
-				      });
-				      
 				      var point = new Graphic({
-					        geometry: geometry,
-					        symbol: symbol.toJson(),
-					        attributes: attr,
-					        infoTemplate: template
+				    	  	geometry: geometry,
+						    symbol: symbol,
+						    attributes: params.attr
+				      	});
+
+				      if(params.template){
+				    	  point.popupTemplate = params.template;
+				      }
+				      
+				      ArGis.view.graphics.add(point);
+			});
+		},
+		drawLine: function(params){
+			require(["esri/Color","esri/Graphic"], 
+					function (Color, Graphic) {
+				 		var geometry = {
+					        type: "polyline", 
+					        paths: params.paths
+					      };
+						
+				      var symbol  = {
+				  		    type: "simple-line",  
+				  		    color: params.color ? new Color(params.color) : [226, 119, 40],
+				  		    width: (params.width ? params.width : 4) + "px",
+				  		    style: params.style ? params.style : "solid"
+				  		  };
+
+				      var line = new Graphic({
+				    	  	geometry: geometry,
+						    symbol: symbol,
+						    attributes: params.attr
 				      	});
 				      
-				      ArGis.map.graphics.add(point);
+				      if(params.template){
+				    	  line.popupTemplate = params.template;
+				      }
+				      
+				      console.log(line);
+				      ArGis.view.graphics.add(line);
 			});
-			 
+		},
+		drawShape: function(params){
+			require(["esri/Color","esri/Graphic"], 
+					function (Color, Graphic) {
+				 		var geometry = {
+					        type: "polyline", 
+					        paths: paths
+					      };
+						
+				      var symbol  = {
+				  		    type: "simple-line",  
+				  		    color: color ? new Color(color) : [226, 119, 40],
+				  		    width: (width ? width : 1) + "px",
+				  		    style: style ? style : "solid"
+				  		  };
+
+				      var point = new Graphic({
+				    	  	geometry: geometry,
+						    symbol: symbol,
+						    attributes: attr,
+						    popupTemplate: template
+				      	});
+				      
+				      ArGis.view.graphics.add(point);
+			});
+		},
+		removeGraphicsByType: function(type) {
+			　var graphics = ArGis.view.graphics;
+			var delArray = [];
+			graphics.forEach(function(item, i){
+			  if(item.attributes && item.attributes.type == type){
+				  delArray.push(item);
+			  }
+			});
+			graphics.removeMany(delArray);
+		},
+		formatDate: function(d, s){
+
+		    var date = new Date();
+		    if(d){
+		        if(typeof d == 'object'){
+		            date = d;
+		        }else{
+		            if(isNaN(d)){
+		                date = new Date(d.replace(/-/g, "/").replace(/年/g, "/").replace(/月/g, "/").replace(/日/g, " ").replace(/时/g, ":").replace(/分/g, ":").replace(/秒/g, ""));
+		            }else{
+		                d = String(d).length == 10 ? d + "000" : String(d).length == 13 ? d : new Date().getTime() + Number(d);
+		                date = new Date(Number(d));
+		            }
+		        }
+		    }
+		    var weekday = ["星期日","星期一","星期二","星期三","星期四","星期五","星期六"];
+		    var weekdayS = ["日","一","二","三","四","五","六"];
+		    var weekdayEn = ["Sunday","Monday","Tuesday","Wednesday","Thursday ","Friday","Saturday"];
+		    var weekdayEnS = ["Sun.","Mon.","Tues.","Wed.","Thurs. ","Fri.","Sat."];
+		    var t = String(s);
+		    t = t.replace('yyyy', date.getFullYear());
+		    t = t.replace('yy', date.getYear);
+		    t = t.replace('MM', (date.getMonth()+1) < 10 ? "0"+(date.getMonth()+1) : (date.getMonth()+1));
+		    t = t.replace('M', (date.getMonth()+1));
+		    t = t.replace('dd', date.getDate() < 10 ? "0"+date.getDate() : date.getDate());
+		    t = t.replace('d', date.getDate());
+		    t = t.replace('HH', date.getHours() < 10 ? "0"+date.getHours() : date.getHours());
+		    t = t.replace('H', date.getHours());
+		    t = t.replace('mm', date.getMinutes() < 10 ? "0"+date.getMinutes() : date.getMinutes());
+		    t = t.replace('m', date.getMinutes());
+		    t = t.replace('ss', date.getSeconds() < 10 ? "0"+date.getSeconds() : date.getSeconds());
+		    t = t.replace('s', date.getSeconds());
+		    t = t.replace('S', date.getMilliseconds());
+		    t = t.replace('en:ww', weekdayEn[date.getDay()]);
+		    t = t.replace('en:w', weekdayEnS[date.getDay()]);
+		    t = t.replace('cn:ww', weekday[date.getDay()]);
+		    t = t.replace('cn:w', weekdayS[date.getDay()]);
+		    t = t.replace('ww', weekday[date.getDay()]);
+		    t = t.replace('w', weekdayS[date.getDay()]);
+		    return t;
 		}
 }
